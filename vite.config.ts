e94e60cdbd87bc48efc776c5df681aa46d9e913b
sneogi2006 <sneogi@aortem.io>
2026-03-10 18:24:11 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  // ADD THIS LINE BELOW:
+  base: "/datfo/", 
+
   server: {
     host: "::",
     port: 8080,
